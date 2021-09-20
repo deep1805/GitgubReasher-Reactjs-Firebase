651566c944f7658087a507256a8af51c4507d9ef
Deep Langhnoja <langhnojadeep1851@gmail.com>
2021-09-20 12:45:58 +0530

    Done

---
+++ src/Config/firebaseConfig.js
@@ -0,0 +1,9 @@
+export default {
+  apiKey: 'AIzaSyDhQhVnhONtzSuiRKLDbYqei4eCsyAPxMk',
+  authDomain: 'mygitapp-e293c.firebaseapp.com',
+  projectId: 'mygitapp-e293c',
+  storageBucket: 'mygitapp-e293c.appspot.com',
+  messagingSenderId: '776325443304',
+  appId: '1:776325443304:web:29a4b5de2419c1505fa35d',
+  measurementId: 'G-KZJGVQ3QW4',
+}
